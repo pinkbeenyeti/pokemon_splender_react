5bd56a04f406658776cacefcc96276d9c6eb176d
pinkbeenyeti <rbgusdn12@gmail.com>
2025-03-27 23:22:37 +0900

    feat: 새로운 경로 추가

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "@/pages/Landing/LandingPage";
 import RedirectPage from "@/pages/Login/RedirectPage";
+import MainPage from "@/pages/Main/MainPage";
+import ProfilePage from "@/pages/Profile/ProfilePage";
+import Layout from "@/components/Layout/Layout";
+import ProtectedRoute from "@/router/Protect/ProtectedRoute";
+import ProtectedNewUserRoute from "./Protect/ProtectedNewUserRoute";
 import RoutePath from "@/router/RouterPath";
 
 function Router() {
@@ -10,6 +15,21 @@ function Router() {
       element: <LandingPage />,
     },
     { path: RoutePath.OAUTH2_LOGIN, element: <RedirectPage /> },
+    {
+      path: "/",
+      element: <ProtectedNewUserRoute />,
+      children: [{ path: RoutePath.PROFILE, element: <ProfilePage /> }],
+    },
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          element: <ProtectedRoute />,
+          children: [{ path: RoutePath.MAIN, element: <MainPage /> }],
+        },
+      ],
+    },
   ]);
 
   return (

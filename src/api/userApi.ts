def764983e759eb5517e1729c942c16644e9e5cb
pinkbeenyeti <rbgusdn12@gmail.com>
2025-04-05 17:43:01 +0900

    feat: api 요청을 보내는 함수 추가가

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,14 +1,28 @@
+import { useEffect, useState } from "react";
 import axios from "axios";
 
-export const userAuthApi = async (): Promise<boolean> => {
-  try {
-    const response = await axios.get("/api/user/authenticated", {
-      withCredentials: true, // 쿠키를 포함한 요청
-    });
+export const useAuthSync = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
 
-    return response.status === 200; // 200 상태 코드가 반환되면 인증된 것으로 판단
-  } catch (error) {
-    console.error(error); // 오류가 발생하면 인증되지 않은 것으로 간주
-    return false;
-  }
+  useEffect(() => {
+    const syncAuthState = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_AUTH_API_URL}`,
+          {
+            withCredentials: true, // 쿠키 포함
+          }
+        );
+
+        setIsAuthenticated(response.status === 200);
+      } catch (error) {
+        console.error("인증 상태 확인 실패:", error);
+        setIsAuthenticated(false);
+      }
+    };
+
+    syncAuthState();
+  }, []);
+
+  return isAuthenticated;
 };

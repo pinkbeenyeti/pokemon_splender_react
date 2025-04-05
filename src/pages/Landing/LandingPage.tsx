b94d49e5005c6bddc33749fff60afa715a6006d7
pinkbeenyeti <rbgusdn12@gmail.com>
2025-04-05 17:36:07 +0900

    feat: 랜딩 페이지에서 로그인 후 리다이렉트 로직 수정사항 반영영

--- src/pages/Landing/LandingPage.tsx
+++ src/pages/Landing/LandingPage.tsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { setRedirectCookie } from "@/utils/cookies";
 
 const LandingPage: React.FC = () => {
   const kakaoUrl = import.meta.env.VITE_KAKAO_URL;
   const googleUrl = import.meta.env.VITE_GOOGLE_URL;
 
+  const redirectToLogin = (baseUrl: string) => {
+    const currentUrl = window.location.href;
+    setRedirectCookie("redirect_uri", currentUrl, 0.02); // 쿠키에 현재 URL 저장
+    window.location.href = `${baseUrl}`;
+  };
+
   return (
     <div>
       <h1>Landing Page</h1>
-      <button onClick={() => (window.location.href = googleUrl)}>
-        Google Login
-      </button>
-      <button onClick={() => (window.location.href = kakaoUrl)}>
-        Kakao Login
-      </button>
+      <button onClick={() => redirectToLogin(googleUrl)}>Google Login</button>
+      <button onClick={() => redirectToLogin(kakaoUrl)}>Kakao Login</button>
     </div>
   );
 };

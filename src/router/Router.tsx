a463547e577a5687399d7376a7e5cb20e4e40721
pinkbeenyeti <rbgusdn12@gmail.com>
2025-04-05 17:39:21 +0900

    fix: 라우팅 변경 적용용

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,24 +1,24 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "@/pages/Landing/LandingPage";
-import RedirectPage from "@/pages/Login/RedirectPage";
 import MainPage from "@/pages/Main/MainPage";
 import ProfilePage from "@/pages/Profile/ProfilePage";
 import Layout from "@/components/Layout/Layout";
 import ProtectedRoute from "@/router/Protect/ProtectedRoute";
 import ProtectedNewUserRoute from "./Protect/ProtectedNewUserRoute";
-import RoutePath from "@/router/RouterPath";
+//import LoadingPage from "@/pages/Loading/LoadingPage";
+import RouterPath from "@/router/RouterPath";
+//import LoginModal from "@/pages/Login/LoginModal";
 
 function Router() {
   const router = createBrowserRouter([
     {
-      path: RoutePath.HOME,
+      path: RouterPath.HOME,
       element: <LandingPage />,
     },
-    { path: RoutePath.OAUTH2_LOGIN, element: <RedirectPage /> },
     {
       path: "/",
       element: <ProtectedNewUserRoute />,
-      children: [{ path: RoutePath.PROFILE, element: <ProfilePage /> }],
+      children: [{ path: RouterPath.PROFILE, element: <ProfilePage /> }],
     },
     {
       path: "/",
@@ -26,7 +26,7 @@ function Router() {
       children: [
         {
           element: <ProtectedRoute />,
-          children: [{ path: RoutePath.MAIN, element: <MainPage /> }],
+          children: [{ path: RouterPath.MAIN, element: <MainPage /> }],
         },
       ],
     },
--- src/router/RouterPath.ts
+++ src/router/RouterPath.ts
@@ -1,6 +1,5 @@
 const RouterPath = {
   HOME: "/",
-  OAUTH2_LOGIN: "/oauth2/redirect",
   MAIN: "/main",
   PROFILE: "/profile",
 };

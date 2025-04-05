87186abe45312b987d1fc291ec444d07e3095c70
pinkbeenyeti <rbgusdn12@gmail.com>
2025-04-05 17:39:48 +0900

    feat: 리다이렉트를 위한 쿠키 함수 추가가

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -5,10 +5,14 @@ export const getCookies = (): Record<string, string> =>
     return acc;
   }, {});
 
-export const setCookie = (name: string, value: string, days = 1) => {
+export const setRedirectCookie = (name: string, value: string, days = 1) => {
   const expires = new Date();
   expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000); // 만료일 계산
   document.cookie = `${name}=${encodeURIComponent(
     value
   )}; expires=${expires.toUTCString()}; path=/`;
+};
+
+export const deleteCookie = (name: string) => {
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
 };

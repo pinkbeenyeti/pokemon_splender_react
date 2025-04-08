5c9dd8e0c9667942ce61d32920b8b53ae318c7a3
pinkbeenyeti <rbgusdn12@gmail.com>
2025-04-08 12:02:39 +0900

    fix: 시간 하즈 코딩 분리리

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,3 +1,5 @@
+const time = 1000 * 60 * 60 * 24; // 1일
+
 export const getCookies = (): Record<string, string> =>
   document.cookie.split("; ").reduce((acc: Record<string, string>, cookie) => {
     const [name, value] = cookie.split("=");
@@ -7,7 +9,7 @@ export const getCookies = (): Record<string, string> =>
 
 export const setRedirectCookie = (name: string, value: string, days = 1) => {
   const expires = new Date();
-  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000); // 만료일 계산
+  expires.setTime(expires.getTime() + days * time); // 만료일 계산
   document.cookie = `${name}=${encodeURIComponent(
     value
   )}; expires=${expires.toUTCString()}; path=/`;
